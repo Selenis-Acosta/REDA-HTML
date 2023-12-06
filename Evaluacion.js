e9a73908b8be6890b6fdba16f49d58b7bde890d3
Leyofmilo <143360110+Leyofmilo@users.noreply.github.com>
2023-12-05 23:53:20 -0500

    Evaluacion terminada

--- Evaluacion.js
+++ Evaluacion.js
@@ -1,9 +1,10 @@
-var salida='';
 function calificacion() {
-   let nota= 0;
-   let Pregunta1= parseInt(document.getElementById('Pregunta1').value);
-   let Pregunta2= parseInt(document.getElementById('Pregunta2').value);
-   let Pregunta3= parseInt(document.getElementById('Pregunta3').value);
+   let nota = 0;
+
+   // Obtener respuestas
+   let Pregunta1 = parseInt(document.getElementById('Pregunta1').value);
+   let Pregunta2 = parseInt(document.getElementById('Pregunta2').value);
+   let Pregunta3 = parseInt(document.getElementById('Pregunta3').value);
    let Pregunta4= (document.getElementById('Pregunta4').value);
    let Pregunta5= (document.getElementById('Pregunta5').value);
    let Pregunta6= (document.getElementById('Pregunta6').value);
@@ -11,54 +12,59 @@ function calificacion() {
    let Pregunta8= (document.getElementById('Pregunta8').value);
    let Pregunta9= (document.getElementById('Pregunta9').value);
    let Pregunta10= (document.getElementById('Pregunta10').value);
-   if (Pregunta1==1752) {
+   // ... (obtener respuestas de Pregunta4 a Pregunta10)
+
+   // Verificar respuestas y calcular la nota
+   if (Pregunta1 === 1752) {
       nota++;
    }
-   if (Pregunta2==18104) {
+   if (Pregunta2 === 18104) {
       nota++;
    }
-   if (Pregunta3==82075) {
+   if (Pregunta3===82075) {
       nota++;
    }
-   if (Pregunta4==300) {
+   if (Pregunta4===300) {
       nota++;
    }
-   if (Pregunta5==3840) {
+   if (Pregunta5===3840) {
       nota++;
    }
-   if (Pregunta6==2310) {
+   if (Pregunta6===2310) {
       nota++;
    }
-   if (Pregunta7==49560) {
+   if (Pregunta7===49560) {
       nota++;
    }
-   if (Pregunta8==659) {
+   if (Pregunta8===659) {
       nota++;
    }
-   if (Pregunta9==235) {
+   if (Pregunta9===235) {
       nota++;
    }
-   if (Pregunta10==1) {
+   if (Pregunta10===1) {
       nota++;
+   }
+
+   // ... (verificar respuestas de Pregunta3 a Pregunta10)
+
+   // Generar salida de respuestas
+   let salida = '';
+   salida += '<tr><td>' + respuesta(Pregunta1, 1752) + '</td><td>' + respuesta(Pregunta2, 18104) + '</td><td>'+respuesta(Pregunta3, 82075)+
+   '</td><td>' + respuesta(Pregunta4, 300) + '</td><td>' + respuesta(Pregunta5, 3840) + '</td><td>'+ respuesta(Pregunta6, 2310) + '</td><td>'
+   + respuesta(Pregunta7, 49560) + '</td><td>'+ respuesta(Pregunta8, 659) + '</td><td>' + respuesta(Pregunta9, 235) + '</td><td>'+ respuesta(Pregunta10, 1) + '</td><td>'
+   + nota + '</td></tr>'
+   
+   
+   // ... (añadir salida para Pregunta3 a Pregunta10)
+
+   document.getElementById('nota').innerHTML = salida;
+}
+
+function respuesta(respuestaUsuario, respuestaCorrecta) {
+   if (respuestaUsuario === respuestaCorrecta) {
+      return '<p style="color: green;">' + respuestaUsuario + ', la respuesta es correcta</p>';
+   } else {
+      return '<p style="color: red;">' + respuestaUsuario + ', la respuesta es incorrecta, la respuesta correcta es <span style="color: green;">' + respuestaCorrecta + '</span></p>';
    }
 }
-function respuesta(Pregunta1) {
-   if (Pregunta1==1752) {
-      text= '<p style="color: green;">'+1752+', la respuesta es correcta</p>';
-  }
-  else {
-      text= '<p style="color: red;">'+Pregunta1+', la respuesta es incorrecta, la respuesta correcta es','<spam style="color: green;>'+1752+'</spam>','</p>';
-  }
-  return text;
-}
-function Pregunta2(Pregunta2) {
-   if (Pregunta2==18104) {
-      text= '<p style="color: green;">'+18104+', la respuesta es correcta</p>';
-  }
-  else {
-      text= '<p style="color: red;">'+Pregunta2+', la respuesta es incorrecta, la respuesta correcta es','<spam style="color: green;>'+18104+'</spam>','</p>';
-  }
-  return text;
-}
-salida+='<tr><td>'+respuesta(Pregunta1)+'</td><td>'+Pregunta2(Pregunta2)+'</td>'
-document.getElementById('nota').innerHTML=salida;
